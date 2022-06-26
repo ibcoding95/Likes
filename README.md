505d99959a6f761cfc461dc9f92f957bc8181764
Ibrahim I <ibrahim@Ibrahims-MacBook-Pro.local>
2022-06-25 21:22:17 -0700

    Likes Assignment

---
+++ README.md
@@ -0,0 +1,19 @@
+Likes Assignment: 
+
+Objectives:
+    Continue practicing the onclick event.
+    Make use of querySelector to select the elements you want to change.
+
+
+Part 1: 
+    Create a user card where each time you click the "like" button it should increase the number of likes by "1"
+
+Part 2: 
+    Create a user card with: 
+        - Avatar User pic icon
+        - "Add an avatar" link
+        - Name on the user card: James Doe
+
+    Create three like buttons for the following users post: Neil, Nichole, and Jim. 
+
+        Clicking each like button will increase number of likes for each post by 1. 
---
+++ script.js
@@ -0,0 +1,35 @@
+var count = 3 
+var countElement = document.querySelector("#like")
+
+function add1() {
+    count++
+    countElement.innerText = count + " like(s)"
+}
+
+// Neil's Likes
+var count2 = 9
+var countElement2 = document.querySelector("#neil-likes")
+
+function add2() {
+    count2++
+    countElement2.innerText = count2 + " like(s)"
+}
+
+// Nichole K
+var count3 = 12
+var countElement3 = document.querySelector("#nichole-likes")
+
+function add3() {
+    count3++
+    countElement3.innerText = count3 + " like(s)"
+}
+
+// Jim R
+
+var count4 = 9
+var countElement4 = document.querySelector("#jim-likes")
+
+function add4() {
+    count4++
+    countElement4.innerText = count4 + " like(s)"
+}
